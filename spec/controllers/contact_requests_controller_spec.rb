ae91e11b13be7d5c161d3306134eee8cdef88d64
konada <konrad.cisak@gmail.com>
2017-10-15 21:17:44 +0200

    Update controller specs

--- spec/controllers/contact_requests_controller_spec.rb
+++ spec/controllers/contact_requests_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ContactRequestsController do
+
   describe 'GET #new' do
     it 'assigns a new Contact to @contact_request' do
       contact_request = FactoryGirl.build(:contact_request)
@@ -18,6 +19,15 @@ RSpec.describe ContactRequestsController do
         expect {
                   post :create, contact_request: (FactoryGirl.build(:contact_request).attributes)
                 }.to change(ContactRequest, :count).by(1)
+      end
+    end
+
+  describe 'GET #index' do
+    it "should let an admin see all the posts" do
+      user = FactoryGirl.create(:user, :admin)
+      sign_in :user, user
+      get :index
+      expect( response ).to render_template( :index )
       end
     end
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,15 @@
 FactoryGirl.define do
   factory :user do
-    
+    email { FFaker::Internet.email }
+    created_at Time.now
+    password 'password1111'
+
+
+    trait :admin do
+    admin true
+    end
+
+    trait :regular_user do
+    end
   end
 end
