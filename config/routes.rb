b8d62c557eae2b348c42d138b96f32e2cf584ddc
konada <konrad.cisak@gmail.com>
2017-10-28 15:22:07 +0200

    Rename controller action from show to new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :contact_requests, only: [:index, :new, :create]
-  resource :complaint
+  resource :complaint, only: [:new, :create]
   root 'contact_requests#new'
   get 'admin', to: 'contact_requests#index'
 
