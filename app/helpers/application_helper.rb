ded156cd46a02a47e6c5fea80377605dedae4a3e
konada <konrad.cisak@gmail.com>
2017-10-29 14:58:04 +0100

    Styling fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
 module ApplicationHelper
-
   def view_object(name, args = {})
-    class_name = name.to_s.titleize.split(" ").join("")
-    class_name.constantize.new(self, args)
+    class_name = name.to_s.titleize.split(' ').join('')
+    class_name.constantize.new(args)
   end
-
 end
