dbac08cfa01421d5ed195d68217e9c8d0107fcd3
konada <konrad.cisak@gmail.com>
2017-10-29 14:57:33 +0100

    Remove unused methods

--- app/view_objects/base_view_object.rb
+++ app/view_objects/base_view_object.rb
@@ -1,16 +1,10 @@
 class BaseViewObject
-  attr_reader :context
-
-  include Rails.application.routes.url_helpers
   include ActionView::Helpers
-  include ActionView::Context
 
-  def initialize(context, args = {})
-    @context = context
+  def initialize(args = {})
     after_init(args)
   end
 
   def after_init(args = {})
-    # implemented by child classes
   end
 end
