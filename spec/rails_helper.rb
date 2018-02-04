d1998fe6965dabf7a9244b520fcb71f13ab02da7
konada <konrad.cisak@gmail.com>
2018-02-04 15:41:50 +0100

    Add s3 stub saving

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,4 +68,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Stub saving files to S3
+  config.before(:each) do
+    allow_any_instance_of(Paperclip::Attachment).to receive(:save).and_return(true)
+  end
 end
